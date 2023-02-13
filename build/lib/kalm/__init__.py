03a61062eaea4b7be59041291514ada455458f52
Jakob Holst <jho@miracle.dk>
2023-02-13 13:05:00 +0000

    init #

---
+++ build/lib/kalm/__init__.py
@@ -0,0 +1,9 @@
+from kalm import kalm
+
+def main():
+    """Entry point for the application script"""
+    print("Call your main application code here")
+    print(kalm.kalm())
+
+
+
---
+++ src/kalm/kalm.py
@@ -0,0 +1,4 @@
+def kalm():
+    myanswer = "Be my guest"
+    print( "Geeks 4 Geeks !")
+    return myanswer
