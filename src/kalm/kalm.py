e0c8a9560e758123d7aa66a5faeffe6f10fff3f8
Jakob Holst <jho@miracle.dk>
2023-03-20 13:27:03 +0100

    demo

--- src/kalm/kalm.py
+++ src/kalm/kalm.py
@@ -10,7 +10,7 @@ import datetime
 def prettyllog(function, action, item, organization, statuscode, text):
   d_date = datetime.datetime.now()
   reg_format_date = d_date.strftime("%Y-%m-%d %I:%M:%S %p")
-  print("%-20s: %-12s %-20s %-50s %-20s %-4s %-50s " %( reg_format_date, function,action,item,organization,statuscode, text))
+  print("%-20s: %-12s %20s %-50s %-20s %-4s %-50s " %( reg_format_date, function,action,item,organization,statuscode, text))
 
 class Hvac:
   def __init__(self):
