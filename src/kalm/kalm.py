ea287b32d0fe3b9a85a18854d0dc57bd2a53ea1b
Jakob Holst <jho@miracle.dk>
2023-03-29 14:25:39 +0200

    demo

--- src/kalm/kalm.py
+++ src/kalm/kalm.py
@@ -170,6 +170,7 @@ def awx_create_inventory(name, description, organization, inventorytype, variabl
     vms = nb.virtualization.virtual_machines.all()
     for vm in vms:
       pri_ip = str(vm.primary_ip)
+      print(vm)
       print(pri_ip)
 
 
