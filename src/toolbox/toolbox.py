e344579a0a775b2a9e7bc5e36aee08186e514e38
Jakob Holst <jho@miracle.dk>
2023-04-21 08:46:47 +0200

    toolbox invetede

---
+++ src/toolbox/toolbox.py
@@ -0,0 +1,8 @@
+import datetime
+
+def prettyllog(function, action, item, organization, statuscode, text):
+  d_date = datetime.datetime.now()
+  reg_format_date = d_date.strftime("%Y-%m-%d %I:%M:%S %p")
+  print("%-20s: %-12s %20s %-50s %-20s %-4s %-50s " %( reg_format_date, function,action,item,organization,statuscode, text))
+
+  
