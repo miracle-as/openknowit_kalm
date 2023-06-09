c1c79061faa24899662fec000905478976e27dca
JakobHolst <jakob.holst@knowit.com>
2023-06-09 10:18:54 +0200

    demo

--- src/kalm/dns/dns.py
+++ src/kalm/dns/dns.py
@@ -18,7 +18,30 @@ def init_redis():
     print("Redis is not working")
     exit(1)
 
-  
+
+def env_check():
+  print("env check")
+  domain = os.getenv('KALM_DNS_DOMAIN')
+  url=os.getenv('KALM_DNS_URL')
+  dns_type=os.getenv('KALM_DNS_TYPE')
+  token=os.getenv('KALM_DNS_TOKEN')
+  if domain is None:
+    print("Error: KALM_DNS_DOMAIN is not set")
+    exit(1)
+  if url is None:
+    print("Error: KALM_DNS_URL is not set")
+    exit(1)
+  if dns_type is None:
+    print("Error: KALM_DNS_TYPE is not set")
+    exit(1)
+  if token is None:
+    print("Error: KALM_DNS_TOKEN is not set")
+    exit(1)
+  print("KALM_DNS_DOMAIN: " + domain)
+  print("KALM_DNS_URL: " + url)
+  print("KALM_DNS_TYPE: " + dns_type)
+  print("KALM_DNS_TOKEN: " + token)
+   
 def list_dns():
     print("list dns")
     domain = os.getenv('KALM_DNS_DOMAIN')
