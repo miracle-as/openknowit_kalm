fc69f9833631d6b7206b142a904703a4844dd4fe
Jakob Holst <jho@miracle.dk>
2023-07-15 22:15:49 +0200

    header var inside func

--- src/kalm/netbox/netbox.py
+++ src/kalm/netbox/netbox.py
@@ -50,23 +50,46 @@ def check_netbox_environment_variables():
 def check_netbox_connectivity():
         status = get_netbox_data("status")
         return status
+def create_netbox_config(organiaztion):
+    config = {}
+    config["netbox"] = {}
+    config["netbox"]["organization"] = organiaztion
+    prettyllog("netbox", "check", "config", "-", "001", "Creating /etc/kalm/netbox.json")
+    open('/etc/kalm/netbox.json', 'w').close()
+    with open('/etc/kalm/netbox.json', 'w') as outfile:
+        json.dump(config, outfile)
 
 
-def read_kalm_netbox_config():
+
+def read_kalm_netbox_config(organization):
     if os.path.isfile('/etc/kalm/netbox.json') == False:
         prettyllog("netbox", "check", "config", "-", "001", "Could not find /etc/kalm/netbox.json")
-        return False
+        create_netbox_config(organization)
     config = read_file('/etc/kalm/netbox.json')
     config = json.loads(config)
     return config
 
-
+def read_kalm_config():
+    if os.path.isfile('/etc/kalm/kalm.json') == False:
+        prettyllog("netbox", "check", "config", "-", "001", "Could not find /etc/kalm/kalm.json")
+        return False
+    config = read_file('/etc/kalm/kalm.json')
+    config = json.loads(config)
+    return config
 
 def service(args):
     while not bauilout:
         prettyllog("netbox", "check", "access", "-", "000", "Read the kalm netbox config")
-        config = read_kalm_netbox_config()
-        print(config)
+        kalmconfig = read_kalm_config()
+        if kalmconfig is not False:
+            prettyllog("netbox", "check", "access", "-", "000", "We have access to /etc/kalm/kalm.json")
+            prettyllog("netbox", "check", "access", "-", "000", "Out organization is %s" % kalmconfig["organization"])
+            netboxconfig = read_kalm_netbox_config(kalmconfig["organization"])
+        else:
+            prettyllog("netbox", "check", "access", "-", "000", "We have no access to /etc/kalm/kalm.json")
+
+
+
         prettyllog("netbox", "check", "access", "-", "000", "Checking netbox environment variables")
         bailout = check_netbox_environment_variables()
         prettyllog("netbox", "check", "access", "-", "000", "Checking netbox connectivity")
