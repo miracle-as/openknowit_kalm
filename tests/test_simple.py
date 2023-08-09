746a152e1f485ad3a16dc6007071c24bf708d69d
Jakob Holst <jakob.holst@knowit.dk>
2023-08-09 06:47:03 -0400

    new releaSE

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -3,15 +3,3 @@
 # setup.py that excludes installing the "tests" package
 
 import unittest
-
-from sample.simple import add_one
-
-
-class TestSimple(unittest.TestCase):
-
-    def test_add_one(self):
-        self.assertEqual(add_one(5), 6)
-
-
-if __name__ == '__main__':
-    unittest.main()
