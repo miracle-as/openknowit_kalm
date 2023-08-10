19eee22730885ade43fd4e7df81954b18a92967a
Jakob Holst <jakob.holst@knowit.dk>
2023-08-10 04:06:31 -0400

    more logging

--- src/kalm/common.py
+++ src/kalm/common.py
@@ -4,8 +4,10 @@ import datetime
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
+    DEBUG   = '\033[94m'
     OKCYAN = '\033[96m'
     OKGREEN = '\033[92m'
+    INFO    = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
