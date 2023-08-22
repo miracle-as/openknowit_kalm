dde3aa9897091528071996f0d3f749938ba027c6
Jakob Holst <lyngknuden@gmail.com>
2023-08-22 10:39:56 +0200

    ssh config

--- src/kalm/dns/dns.py
+++ src/kalm/dns/dns.py
@@ -369,7 +369,7 @@ def virtlib(args):
     for ip4 in ip4s:
       prettyllog("manage", "dns", domain_name, "new", "000", "add dns record %s" % (ip4["domain_name"] + "." + ip4["network"] + ".openknowit.com"))
       add_dns_record(ip4["domain_name"], "A", ip4["ipaddress"])
-      prettyllog("manage", "dns", domain_name, "new", "000", "add fingerprint record %s" % (ip4["domain_name"] + "." + ip4["network"] + ".openknowit.com"))
-      add_dns_record(ip4["domain_name"], "TXT", ip4['fingerprint'])
+      #prettyllog("manage", "dns", domain_name, "new", "000", "add fingerprint record %s" % (ip4["domain_name"] + "." + ip4["network"] + ".openknowit.com"))
+      #add_dns_record(ip4["domain_name"], "TXT", ip4['fingerprint'])
 
 
