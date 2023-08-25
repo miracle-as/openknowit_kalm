0013f948924b6d8c8c6ce5b50d2dbec0baa4f1ca
Jakob Holst <lyngknuden@gmail.com>
2023-08-25 10:59:32 +0200

    url incvalis

--- src/kalm/dns/dns.py
+++ src/kalm/dns/dns.py
@@ -255,7 +255,7 @@ def get_records():
     for record in records:
       if record['type'] == 'A':
          print(record['value'] + " " + record['name'])
-    return True
+    return records
 
 
 def list_interfaces():
