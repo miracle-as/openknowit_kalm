4c017410e929e95d9025b5fbbf0fef6bd8c8b7f5
Jakob Holst <jho@miracle.dk>
2023-09-27 01:35:46 +0200

    patch

--- src/kalm/bump/bump.py
+++ src/kalm/bump/bump.py
@@ -1,48 +1,55 @@
 import os
 from pprint import pprint
+import toml 
 
-def prebump():
-    #check if pyproject.toml exists
-    if os.path.isfile('pyproject.toml'):
-        print("pyproject.toml exists")
-    else:
-        print("pyproject.toml does not exist")
-        exit(1)
-    #check if pyproject.toml contains poetry section
+def major():  
+    print("major")
+    with open('pyproject.toml', 'r') as toml_file:
+        data = toml.load(toml_file)
+    # Bump the version numbers
+    if 'tool' in data and 'poetry' in data['tool'] and 'version' in data['tool']['poetry']:
+        current_version = data['tool']['poetry']['version']
+        parts = current_version.split('.')
+        if len(parts) == 3:
+            major, minor, patch = map(int, parts)
+            major += 1
+            new_version = f'{major}.{minor}.{patch}'
+            data['tool']['poetry']['version'] = new_version
+    # Save the modified pyproject.toml file
+    with open('pyproject.toml', 'w') as toml_file:
+        toml.dump(data, toml_file)
 
-    #check if pyproject.toml contains poetry section
-    read_file = open('pyproject.toml', 'r')
-    lines = read_file.readlines()
-    read_file.close()
-    poetry_section = False
-    poetry_section = []
-    for line in lines:
-        if line.startswith == "[tool.poetry":
-            poetry_section = True
-        if poetry_section:
-            poetry_section.append(line)
-            if line.strip() == "[" and not line.startswith == "[tool.poetry":
-                poetry_section = False
-    print(poetry_section)
-    if poetry_section:
-        print("poetry section exists")
-    else:
-        print("poetry section does not exist")
+def minor():
+    print("minor")
+    with open('pyproject.toml', 'r') as toml_file:
+        data = toml.load(toml_file)
+    # Bump the version numbers
+    if 'tool' in data and 'poetry' in data['tool'] and 'version' in data['tool']['poetry']:
+        current_version = data['tool']['poetry']['version']
+        parts = current_version.split('.')
+        if len(parts) == 3:
+            major, minor, patch = map(int, parts)
+            minor += 1
+            new_version = f'{major}.{minor}.{patch}'
+            data['tool']['poetry']['version'] = new_version
+    # Save the modified pyproject.toml file
+    with open('pyproject.toml', 'w') as toml_file:
+        toml.dump(data, toml_file)
 
-    #check if pyproject.toml contains project section
-    project_section = False
-    project_section = []
-    for line in lines:
-        if line.startswith == "[project":
-            project_section = True
-        if project_section:
-            project_section.append(line)
-            if line.strip() == "[" and not line.startswith == "[project":
-                project_section = False
-    print(project_section)
-    if project_section:
-        print("project section exists")
-    else:
-        print("project section does not exist")
+def patch():
+  with open('pyproject.toml', 'r') as toml_file:
+    data = toml.load(toml_file)
 
-        
+  # Bump the version numbers
+  if 'tool' in data and 'poetry' in data['tool'] and 'version' in data['tool']['poetry']:
+    current_version = data['tool']['poetry']['version']
+    parts = current_version.split('.')
+    if len(parts) == 3:
+        major, minor, patch = map(int, parts)
+        patch += 1
+        new_version = f'{major}.{minor}.{patch}'
+        data['tool']['poetry']['version'] = new_version
+
+  # Save the modified pyproject.toml file
+  with open('pyproject.toml', 'w') as toml_file:
+    toml.dump(data, toml_file)
