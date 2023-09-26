fd841f01026eb887ae66422a960b7d336d20a52e
Jakob Holst <jho@miracle.dk>
2023-09-27 01:41:15 +0200

    demo

--- src/kalm/bump/bump.py
+++ src/kalm/bump/bump.py
@@ -7,12 +7,27 @@ def major():
     with open('pyproject.toml', 'r') as toml_file:
         data = toml.load(toml_file)
     # Bump the version numbers
+    if 'project' in data and 'version' in data['project']:
+        current_version = data['project']['version']
+        parts = current_version.split('.')
+        if len(parts) == 3:
+            major, minor, patch = map(int, parts)
+            major += 1
+            minor = 0
+            patch = 0
+
+            new_version = f'{major}.{minor}.{patch}'
+            data['project']['version'] = new_version
+
     if 'tool' in data and 'poetry' in data['tool'] and 'version' in data['tool']['poetry']:
         current_version = data['tool']['poetry']['version']
         parts = current_version.split('.')
         if len(parts) == 3:
             major, minor, patch = map(int, parts)
             major += 1
+            minor = 0
+            patch = 0
+
             new_version = f'{major}.{minor}.{patch}'
             data['tool']['poetry']['version'] = new_version
     # Save the modified pyproject.toml file
@@ -24,12 +39,25 @@ def minor():
     with open('pyproject.toml', 'r') as toml_file:
         data = toml.load(toml_file)
     # Bump the version numbers
+    if 'project' in data and 'version' in data['project']:
+        current_version = data['project']['version']
+        parts = current_version.split('.')
+        if len(parts) == 3:
+            major, minor, patch = map(int, parts)
+            minor += 1
+            patch = 0
+
+            new_version = f'{major}.{minor}.{patch}'
+            data['project']['version'] = new_version
+
     if 'tool' in data and 'poetry' in data['tool'] and 'version' in data['tool']['poetry']:
         current_version = data['tool']['poetry']['version']
         parts = current_version.split('.')
         if len(parts) == 3:
             major, minor, patch = map(int, parts)
-            minor += 1
+            minor += 1  
+            patch = 0
+            
             new_version = f'{major}.{minor}.{patch}'
             data['tool']['poetry']['version'] = new_version
     # Save the modified pyproject.toml file
@@ -41,6 +69,15 @@ def patch():
     data = toml.load(toml_file)
 
   # Bump the version numbers
+  if 'project' in data and 'version' in data['project']:
+    current_version = data['project']['version']
+    parts = current_version.split('.')
+    if len(parts) == 3:
+      major, minor, patch = map(int, parts)
+      patch += 1
+      new_version = f'{major}.{minor}.{patch}'
+      data['project']['version'] = new_version
+
   if 'tool' in data and 'poetry' in data['tool'] and 'version' in data['tool']['poetry']:
     current_version = data['tool']['poetry']['version']
     parts = current_version.split('.')
