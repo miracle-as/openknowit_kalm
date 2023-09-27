6d3b54f290e3df5cc46e76f72b10f7a51b2a0a3f
jakob holst <jakob.holst@knowit.dk>
2023-09-27 14:03:48 +0200

    bump

--- src/kalm/zabbix/zabbix.py
+++ src/kalm/zabbix/zabbix.py
@@ -55,7 +55,29 @@ def list_host_groups():
     
     print(json.dumps(r.json(), indent=4, sort_keys=True))
 
+def get_host_id(hostname):
+    r = requests.post(ZABBIX_API_URL,
+    json= {
 
+            "jsonrpc": "2.0",
+            "method": "host.get",
+            "params": {
+                "output": "extend",
+                "filter": {
+                    "host": [
+                        hostname
+                    ]
+                }
+            },
+            "auth": AUTHTOKEN,
+            "id": 1
+        })
+    try:
+        return r.json()['result'][0]['hostid']
+    except:
+        print("no host found")
+        return None
+    
 def get_host_group_id(hostgroup):
     r = requests.post(ZABBIX_API_URL,
     json= {
@@ -78,7 +100,45 @@ def get_host_group_id(hostgroup):
     except:
         print("no hostgroup found")
         return None
-    
+def create_host(hostname, hostgroup_id):    
+    r = requests.post(ZABBIX_API_URL,
+    json= {     
+          "jsonrpc": "2.0",     
+          "method": "host.create",     
+          "params": {         
+          "host": hostname,
+          "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 1,
+                "ip": ipadress,
+                "dns": "",
+                "port": "10050"
+            }
+        ],
+        "groups": [
+            {
+                "groupid": hostgroup_id
+            }
+        ],
+        "tags": [
+            {
+                "tag": "Host name",
+                "value": hostname
+            }
+        ],
+        "inventory_mode": 0,
+        "inventory": {
+            "macaddress_a": "01234",
+            "macaddress_b": "56768"
+        }
+          },
+        "id": 1,
+        "auth": AUTHTOKEN
+    })
+
+
 
 def create_host_group(hostgroup = "Linux servers"):
     r = requests.post(ZABBIX_API_URL,
@@ -104,6 +164,14 @@ def status():
 def register():
     print("register")
     try:
+        hostname = os.environ.get('KALM_ZABBIX_HOSTNAME')
+    except:
+        pass
+    if hostname == None:
+        print("no hostname defined in env KALM_ZABBIX_HOSTNAME")
+        return 1
+
+    try:
         hostgroup = os.environ.get('KALM_ZABBIX_HOSTGROUP')
     except:
         pass
@@ -125,6 +193,19 @@ def register():
         print("hostgroup found")
 
     print("hostgroup_id: " + hostgroup_id)
+    host_id = get_host_id(hostname)
+    if host_id == None:
+        print("no host found")
+        host_id = create_host(hostname, hostgroup_id)
+        if host_id == None:
+            print("no host created")
+            return 1
+        else:
+            print("host created")
+    else:
+        print("host found")
+    return 0
+
 
 #    create_host_group()
 #    create_host()
