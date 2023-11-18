3fee665ce1c5017a27c40d1b3a1182f714fb4602
jakob Holst <lyngknuden@gmail.com>
2023-11-18 22:21:06 +0100

    demo

--- src/kalm/semaphore/serve.py
+++ src/kalm/semaphore/serve.py
@@ -10,20 +10,17 @@ def check_env():
 
 def serve():
     print("serve")
-    if not check_env():
-        print("check_env failed")
-        SystemExit(1)
-        
-    print("check_env ok")
-    print("KALM_SEMAPHORE_URL: " + os.getenv('KALM_SEMAPHORE_URL'))
+    if check_env():
+        print("check_env ok")
+        print("KALM_SEMAPHORE_URL: " + os.getenv('KALM_SEMAPHORE_URL'))
 
-    url = os.getenv('KALM_SEMAPHORE_URL') + "/api/v2/projects"
-    print(url)
-    r = requests.get(url)
-    print(r.status_code)
-    print(r.headers)
-    print(r.text)
-    print(r.json)
+        url = os.getenv('KALM_SEMAPHORE_URL') + "/api/v2/projects"
+        print(url)
+        r = requests.get(url)
+        print(r.status_code)
+        print(r.headers)
+        print(r.text)
+        print(r.json)
 
 
         
