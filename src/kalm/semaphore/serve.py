1395e36ea69fe1746f762426221dcb3cd2c1a28d
jakob Holst <lyngknuden@gmail.com>
2023-11-18 22:39:41 +0100

    demo

--- src/kalm/semaphore/serve.py
+++ src/kalm/semaphore/serve.py
@@ -1,26 +1,23 @@
 import requests
 import os
+from ..common import prettyllog
 
 def check_env():
     print("check_env")
     if (os.getenv('KALM_SEMAPHORE_URL') == None):
         print("KALM_SEMAPHORE_URL not set")
         return 1
+    
+def get_projects():
+    print("get_projects")
+    projects = requests.get(os.getenv('KALM_SEMAPHORE_URL') + "/api/v4/projects")
+    return 0
 
 
 def main():
     print("serve")
     if check_env():
-        print("check_env ok")
-        print("KALM_SEMAPHORE_URL: " + os.getenv('KALM_SEMAPHORE_URL'))
-
-        url = os.getenv('KALM_SEMAPHORE_URL') + "/api/v2/projects"
-        print(url)
-        r = requests.get(url)
-        print(r.status_code)
-        print(r.headers)
-        print(r.text)
-        print(r.json)
+        prettyllog("check_env", "KALM_SEMAPHORE_URL", "not set", "set KALM_SEMAPHORE_URL", "error", "error")
 
 
         
