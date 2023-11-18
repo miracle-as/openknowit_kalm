8fa816b7496a8756d4a42fb64a7da837b47591ac
jakob Holst <lyngknuden@gmail.com>
2023-11-18 23:09:13 +0100

    demo

--- src/kalm/semaphore/serve.py
+++ src/kalm/semaphore/serve.py
@@ -2,6 +2,35 @@ import requests
 import os
 from ..common import prettyllog
 
+def login():
+    baseurl = os.getenv('KALM_SEMAPHORE_URL')
+    user = os.getenv('KALM_SEMAPHORE_USER')
+    password = os.getenv('KALM_SEMAPHORE_PASSWORD')
+    url = f"{baseurl}/api/auth/login"
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'auth': user,
+        'password': password
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    # Check the response
+    if response.status_code == 200:
+        # Successful request
+        print("Login successful")
+        print("Response JSON:", response.json())
+        return True
+    else:
+        # Failed request
+        print(f"Error: {response.status_code}")
+        print("Response text:", response.text)
+        return False
+
+
 def check_env():
     print("check_env")
     if (os.getenv('KALM_SEMAPHORE_URL') == None):
@@ -12,24 +41,8 @@ def check_env():
         return 0
     
 
-    
-
-    
-def get_projects():
-    print("get_projects")
-    projects = requests.get(os.getenv('KALM_SEMAPHORE_URL') + "/api/v4/projects")
-    return projects.json()
-
+ # Example: Get a list of your projects
 
 def main():
-    print("serve")
-    if check_env():
-        prettyllog("check_env", "KALM_SEMAPHORE_URL", "not set", "set KALM_SEMAPHORE_URL", "error", "error")
-    projects = get_projects()
-    print(projects)
-
-
-
-
-        
+    login()
     return 0
