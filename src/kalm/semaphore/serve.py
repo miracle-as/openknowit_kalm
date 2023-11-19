42ac335fcf9ef6a72bafec68fe31defdec0b24f6
jakob Holst <lyngknuden@gmail.com>
2023-11-19 21:58:51 +0100

    demo

--- src/kalm/semaphore/serve.py
+++ src/kalm/semaphore/serve.py
@@ -4,6 +4,12 @@ from ..common import prettyllog
 import pprint
 
 
+
+
+
+import os
+import requests
+
 def login():
     baseurl = os.getenv('KALM_SEMAPHORE_URL')
     user = os.getenv('KALM_SEMAPHORE_USER')
@@ -18,33 +24,38 @@ def login():
         'password': password
     }
 
-    response = requests.post(url, headers=headers, json=data)
+    # Create a session
+    session = requests.Session()
+
+    # Make the login request
+    response = session.post(url, headers=headers, json=data)
     if response.status_code == 204:
         # Successful request
         print("Login successful")
-        return True
+        return session  # Return the session for subsequent requests
     else:
         # Failed request
         print(f"Error: {response.status_code}")
-        return False
-def get_projects():
+        return None
+
+def get_project(session):
     baseurl = os.getenv('KALM_SEMAPHORE_URL')
-    user = os.getenv('KALM_SEMAPHORE_USER')
-    password = os.getenv('KALM_SEMAPHORE_PASSWORD')
-    url = f"{baseurl}/api/projects"
+    project_url = f"{baseurl}/api/projects"  # Adjust the URL as needed
     headers = {
         'accept': 'application/json',
         'Content-Type': 'application/json'
     }
-    data = {
-        'auth': user,
-        'password': password
-    }
-    
-    response = requests.get(url, headers=headers)
-    pprint.pprint(response.status_code)
 
-    
+    # Use the session for the request
+    response = session.get(project_url, headers=headers)
+    if response.status_code == 200:
+        # Successful request
+        projects = response.json()
+        print("Projects:", projects)
+    else:
+        # Failed request
+        print(f"Error: {response.status_code}")
+
     
 def check_env():
     print("check_env")
@@ -59,7 +70,9 @@ def check_env():
  # Example: Get a list of your projects
 
 def main():
-    login()
-    get_projects()
-    
+    session = login()
+    if session:
+        get_project(session)
+
+
     return 0
