fe283a56d6f72f7961da389bea5587a19eee50f8
jakob Holst <lyngknuden@gmail.com>
2023-11-19 22:28:05 +0100

    demo

--- src/kalm/semaphore/serve.py
+++ src/kalm/semaphore/serve.py
@@ -51,31 +51,51 @@ def get_project(session):
     if response.status_code == 200:
         # Successful request
         projects = response.json()
-        print("Projects:", projects)
         # map prokects by name
         projects_by_name = {}
         for project in projects:
             projects_by_name[project['name']] = project
-        print("Projects by name:", projects_by_name)
         return projects_by_name
-
     else:
         # Failed request
         print(f"Error: {response.status_code}")
 
-def get_project_by_name(session, project_name):
+def get_inventory(session, project_id):
     baseurl = os.getenv('KALM_SEMAPHORE_URL')
-    project_url = f"{baseurl}/api/projects"  # Adjust the URL as needed
+    inventory_url = f"{baseurl}/api/projects/{project_id}/inventory"  # Adjust the URL as needed
     headers = {
         'accept': 'application/json',
         'Content-Type': 'application/json'
     }
 
     # Use the session for the request
-    response = session.get(project_url, headers=headers)
+    response = session.get(inventory_url, headers=headers)
     if response.status_code == 200:
         # Successful request
-        projects = response.json()
+        inventory = response.json()
+        # map prokects by name
+        inventory_by_name = {}
+        for item in inventory:
+            inventory_by_name[item['name']] = item
+        return inventory_by_name
+    else:
+        # Failed request
+        print(f"Error: {response.status_code}")
+
+def get_inventory_item(session, project_id, inventory_id):
+    baseurl = os.getenv('KALM_SEMAPHORE_URL')
+    inventory_url = f"{baseurl}/api/projects/{project_id}/inventory/{inventory_id}"  # Adjust the URL as needed
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+
+    # Use the session for the request
+    response = session.get(inventory_url, headers=headers)
+    if response.status_code == 200:
+        # Successful request
+        inventory = response.json()
+        return inventory
     else:
         # Failed request
         print(f"Error: {response.status_code}")
@@ -98,6 +118,10 @@ def main():
     if session:
         projects = get_project(session)
         print("Projects:", projects)
+
+        inventory = get_inventory(session, projects['kalm']['id'])
+        print("Inventory:", inventory)
+        
 
 
     return 0
