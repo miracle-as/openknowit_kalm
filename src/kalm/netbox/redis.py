c416fff9554ae9ab72b58ce1cb8fb010ffe5d023
Jakob Holst <jho@miracle.dk>
2023-12-02 22:01:00 +0100

    no tmp

--- src/kalm/netbox/redis.py
+++ src/kalm/netbox/redis.py
@@ -3,6 +3,7 @@ import redis
 def refresh_netbox_from_redis(myenv):
     r = redis.Redis(host='localhost', port=6379, db=0)
     data = {}
+    details = {}
     mykey = "netbox"
     knownservers = r.keys("kalm:vmware:*:known")
     detailedservers = r.keys("kalm:vmware:*:details")
@@ -13,6 +14,6 @@ def refresh_netbox_from_redis(myenv):
     for server in detailedservers:
         key = server.decode("utf-8")
         value = r.get(key)
-        data[key] = value.decode("utf-8")
+        details[key] = value.decode("utf-8")
     print(data)
     return data
