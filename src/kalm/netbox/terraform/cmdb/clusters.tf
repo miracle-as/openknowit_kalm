6fc9cb0ddfa0fbbba1a72ff09b15c0c1dc42bd09
Jakob Holst <jho@miracle.dk>
2024-01-04 08:54:54 +0100

    demotf

--- src/kalm/netbox/terraform/cmdb/clusters.tf
+++ src/kalm/netbox/terraform/cmdb/clusters.tf
@@ -1,16 +1,16 @@
-resource "netbox_cluster" "openstack" {
+resource "netbox_cluster" "openstackcluster01" {
   cluster_type_id  = netbox_cluster_type.openstack.id
   name             = "openstackcluster01"
   cluster_group_id = netbox_cluster_group.openstackclusters.id
 }
 
-resource "netbox_cluster" "vmwareclusters" {
+resource "netbox_cluster" "vmwarecluster01" {
   cluster_type_id  = netbox_cluster_type.vmware.id
   name             = "vmwarecluster01"
   cluster_group_id = netbox_cluster_group.vmwareclusters.id
 }
 
-resource "netbox_cluster" "linuxclusters" {
+resource "netbox_cluster" "linuxcluster01" {
   cluster_type_id  = netbox_cluster_type.vmware.id
   name             = "linuxcluster01"
   cluster_group_id = netbox_cluster_group.linuxclusters.id
