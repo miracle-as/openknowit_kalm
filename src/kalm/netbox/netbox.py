ce4d80c7e2db1eac5b78c849c45d44f32cb3ba52
Jakob Holst <jho@miracle.dk>
2024-01-04 11:53:11 +0100

    3.11.53

--- src/kalm/netbox/netbox.py
+++ src/kalm/netbox/netbox.py
@@ -96,6 +96,7 @@ def addtagtovm(vmname, tag):
         "Accept": "application/json"
     }
     response = requests.get(url, headers=headers, verify=False)
+    pprint.pprint(url)
     pprint.pprint(response.content)
     if response.status_code == 200:
         data = response.json()
