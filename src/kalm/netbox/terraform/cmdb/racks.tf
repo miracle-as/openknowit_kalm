0e2ec91e03ec5e28a35e77d39f7286d24a861392
Jakob Holst <jho@miracle.dk>
2024-01-04 08:38:10 +0100

    demotf

--- src/kalm/netbox/terraform/cmdb/racks.tf
+++ src/kalm/netbox/terraform/cmdb/racks.tf
@@ -1,7 +1,7 @@
 resource "netbox_rack" "demo_rack1" {
   count  =  6
   name         = "demo${format("%02d", count.index + 1)}_rack1"
-  site_id  = netbox_site.demo[count.index].id
+  site_id  = netbox_site.demosite[count.index].id
   status   = "reserved"
   width    = 19
   u_height = 48
