9286fb64dd78aa489aac48e59db72e6c72a09fd6
Jakob Holst <jho@miracle.dk>
2024-01-06 23:52:59 +0100

    read the host groups in conf

--- src/kalm/zabbix/zabbix.py
+++ src/kalm/zabbix/zabbix.py
@@ -15,7 +15,8 @@ AUTHTOKEN =  myenv['KALM_ZABBIX_TOKEN']
 
 
 def list_host_group(hostgroup):
-    prettyllog("zabbix", "list_host_group", "zabbix", "000", "list_host_group for %s " % hostgroup, "info")
+    hostgroup_name = hostgroup['name']
+    prettyllog("zabbix", "list_host_group", "zabbix", "000", "list_host_group for %s " % hostgroup_name, "info")
     r = requests.post(ZABBIX_API_URL,
     json= {     
           "jsonrpc": "2.0",     
@@ -24,7 +25,7 @@ def list_host_group(hostgroup):
           "output": "extend",
                 "filter": {
                 "name": [
-                           hostgroup
+                           hostgroup_name
                         ]         
                 }
 
