76c614c5361441fab4f0e9631c8a4fce6de4b88a
Jakob Holst <jho@miracle.dk>
2024-01-06 21:43:26 +0100

    libvirt support - main added

--- src/kalm/netbox/netbox.py
+++ src/kalm/netbox/netbox.py
@@ -1749,6 +1749,7 @@ def get_virtual_machines(env = get_env()):
         # we need to check if there are more pages 
         if vms['next'] is not None:
             url = vms['next']
+            pprint.pprint(vms)
         else:
             morepages = False 
     return returnvms
