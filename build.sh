55fbef9b073f8a7a2428bdab7896d371299444d7
Jakob Holst <jho@miracle.dk>
2024-01-23 07:00:00 +0100

    build scrpt

---
+++ build.sh
@@ -0,0 +1,12 @@
+kalm_bump patch
+MYMESSAGE=$(git status)
+git commit -a -m  "$MYMESSAGE"
+git push 
+rm poetry.lock
+rm -r dist || echo
+poetry update
+poetry build
+poetry run twine upload dist/* || echo
+#curl -X POST ${IGN8_JENKINS_URL}/job/ign8/build \
+#  --user jho:${IGN8_JENKINS_TOKEN}
+
